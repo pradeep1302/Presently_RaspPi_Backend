9331eeb0e31c69a8e66a452d01fa94c19019f4f6
Pradeep Agrawal <pkagrawal181@gmail.com>
2024-11-08 18:50:57 +0530

    handled functionality

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.get("/", (req, res) => {
 });
 
 app.use(errorHandler);
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 mongoose
 	.connect(process.env.MONGO_URI)
 	.then(() => {
